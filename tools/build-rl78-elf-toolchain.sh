35813356e76e7210bf7511f17ca07e199ea51a34
edgetriggered <110491646+edgetriggered@users.noreply.github.com>
2023-06-30 00:40:46 +0300

    Add script to build RL78 toolchain

---
+++ tools/build-rl78-elf-toolchain.sh
@@ -0,0 +1,88 @@
+#!/usr/bin/env -S bash -x
+
+ctr=$(buildah from docker.io/library/alpine:3.18)
+
+name=rl78-elf-toolchain
+user=toolbox
+
+makdeps="build-base gmp-dev mpfr-dev mpc1-dev zlib-dev diffutils automake texinfo"
+rundeps="sudo gmp mpfr mpc1 make libstdc++ nano git"
+
+binutils_ver="2.40"
+binutils_uri="https://ftp.gnu.org/gnu/binutils/binutils-${binutils_ver}.tar.gz"
+
+gcc_ver="13.1.0"
+gcc_uri="https://ftp.gnu.org/gnu/gcc/gcc-${gcc_ver}/gcc-${gcc_ver}.tar.gz"
+
+gdb_ver="13.1"
+gdb_uri="https://sourceware.org/pub/gdb/releases/gdb-${gdb_ver}.tar.gz"
+
+target=rl78-elf
+tmp="/tmp"
+workdir="/build"
+prefix="/usr/local"
+njobs="$(nproc)"
+
+buildah config --annotation com.github.build.host=$(uname -n) $ctr
+
+buildah run $ctr -- apk add --no-cache $makdeps $rundeps
+
+buildah run $ctr -- adduser -D $user
+buildah run $ctr -- sh -c "echo '$user ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers.d/toolbox"
+
+buildah add $ctr $binutils_uri $tmp
+buildah add $ctr $gcc_uri      $tmp
+buildah add $ctr $newlib_uri   $tmp
+buildah add $ctr $gdb_uri      $tmp
+
+buildah config --workingdir $workdir $ctr
+buildah run $ctr -- tar xf "${tmp}/binutils-${binutils_ver}.tar.gz"
+buildah run $ctr -- rm     "${tmp}/binutils-${binutils_ver}.tar.gz"
+buildah run $ctr -- tar xf "${tmp}/gcc-${gcc_ver}.tar.gz"
+buildah run $ctr -- rm     "${tmp}/gcc-${gcc_ver}.tar.gz"
+buildah run $ctr -- tar xf "${tmp}/newlib-${newlib_ver}.tar.gz"
+buildah run $ctr -- rm     "${tmp}/newlib-${newlib_ver}.tar.gz"
+buildah run $ctr -- tar xf "${tmp}/gdb-${gdb_ver}.tar.gz"
+buildah run $ctr -- rm     "${tmp}/gdb-${gdb_ver}.tar.gz"
+
+buildah config --workingdir "${workdir}/binutils-${binutils_ver}/build" $ctr
+buildah run $ctr -- ../configure \
+	--target=${target} \
+	--prefix=${prefix} \
+	--disable-nls \
+	--with-system-zlib
+buildah run $ctr -- make -j${njobs}
+buildah run $ctr -- make install
+
+buildah config --workingdir "${workdir}/gcc-${gcc_ver}/build" $ctr
+buildah run $ctr -- ../configure \
+	--target=${target} \
+	--prefix=${prefix} \
+	--enable-languages=c \
+	--disable-libssp \
+	--disable-nls \
+	--disable-threads \
+	--disable-libgomp \
+	--disable-libmudflap \
+	--disable-libstdcxx-pch \
+	--disable-multilib \
+	--enable-lto \
+	--with-system-zlib
+buildah run $ctr -- make -j${njobs}
+buildah run $ctr -- make install
+
+buildah config --workingdir "${workdir}/gdb-${gdb_ver}/build" $ctr
+buildah run $ctr -- ../configure \
+	--target=${target} \
+	--prefix=${prefix}
+buildah run $ctr -- make -j${njobs}
+buildah run $ctr -- make install
+
+buildah config --workingdir "/home/${user}" $ctr
+buildah run $ctr -- rm -rf "${workdir}"
+buildah run $ctr -- apk del $makdeps
+
+buildah config --user $user $ctr
+
+buildah commit --rm $ctr $name:latest
+
